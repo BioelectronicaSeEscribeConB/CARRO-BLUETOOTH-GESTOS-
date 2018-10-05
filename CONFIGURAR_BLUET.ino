cfd57c804b9c1165108ccc02c0f70dc168e6042d
Bioelectronica Se escribe con B <35161121+BioelectronicaSeEscribeConB@users.noreply.github.com>
2018-10-05 13:31:04 -0500

    Add files via upload

---
+++ CONFIGURAR_BLUET.ino
@@ -0,0 +1,23 @@
+#include <SoftwareSerial.h>
+SoftwareSerial mySerial(10, 11); // TX, RX
+
+void setup() 
+{
+  Serial.begin(9600);
+  mySerial.begin(38400);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+  Serial.println("Configuracion:");
+}
+
+void loop() 
+{ 
+  if (mySerial.available()) {
+    Serial.write(mySerial.read());
+  }
+  if (Serial.available()) {
+    mySerial.write(Serial.read());
+  }
+}
+
